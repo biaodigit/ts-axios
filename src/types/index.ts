bbdf22a4ef69a162d12c691091c0dc800df91187
biaodigit <15622285015@163.com>
2020-05-20 21:37:31 +0800

    feat:buildURL

---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+export interface AxiosRequestConfig {
+    url: string
+    method?: Method
+    data?:any
+    params?:any
+}
+
+export type Method = 'get' | 'GET' | 
+'post' | 'POST' | 
+'delete' | 'DELETE' |
+'put' | 'PUT' |
+'patch' | 'PATCH' |
+'head' | 'HEAD' |
+'options' | 'OPTIONS'  
