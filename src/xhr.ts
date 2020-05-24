97a5f8776929b9820da0d90729d9c23d605a19d6
biaodigit <15622285015@163.com>
2020-05-24 10:37:33 +0800

    feat:process headers

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,11 +1,17 @@
 import { AxiosRequestConfig } from './types'
 
 export default function xhr(config: AxiosRequestConfig) {
-    const { method = 'get', data = null, url } = config
-
+    const { method = 'get', data = null, url, headers } = config
     const request = new XMLHttpRequest()
+    request.open(method.toUpperCase(), url, true)
 
-    request.open(method.toUpperCase(),url,true)
+    Object.keys(headers).forEach(name => {
+        if (data === null && name.toLowerCase() === 'content-type') {
+            delete headers[name]
+        } else {
+            request.setRequestHeader(name, headers[name])
+        }
+    })
 
     request.send(data)
 }
