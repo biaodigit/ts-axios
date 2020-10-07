a4dfe3b04718bef45bee51984db3086bd1815431
biaodigit <15622285015@163.com>
2020-10-07 13:23:06 +0800

    feat:interceptor

--- src/core/Axios.ts
+++ src/core/Axios.ts
@@ -1,9 +1,27 @@
-import { AxiosPromise, AxiosRequestConfig, Method } from "../types";
+import { AxiosPromise, AxiosRequestConfig, AxiosResponse, Method, ResolvedFn, RejectedFn } from "../types";
 import dispatchRequest from './dispatchRequest'
+import InterceptorManager from './InterceptorManager'
+
+interface Interceptors {
+    request: InterceptorManager<AxiosRequestConfig>
+    response: InterceptorManager<AxiosResponse>
+}
+
+interface PromiseChain<T> {
+    resolved: ResolvedFn<T> | ((config: AxiosRequestConfig) => AxiosPromise)
+    rejected?: RejectedFn
+}
 
 export default class {
+    public interceptors: Interceptors
+
+    constructor() {
+        this.interceptors = {
+            request: new InterceptorManager<AxiosRequestConfig>(),
+            response: new InterceptorManager<AxiosResponse>()
+        }
+    }
     request(url: any, config?: any): AxiosPromise {
-        console.log(this)
         if (typeof url === 'string') {
             if (!config) {
                 config = {}
@@ -12,7 +30,28 @@ export default class {
         } else {
             config = url
         }
-        return dispatchRequest(config)
+
+        const chain: Array<PromiseChain<any>> = [{
+            resolved: dispatchRequest,
+            rejected: undefined
+        }]
+
+        this.interceptors.request.forEach(interceptor => {
+            chain.unshift(interceptor)
+        })
+
+        this.interceptors.response.forEach(interceptor => {
+            chain.push(interceptor)
+        })
+
+        let promise = Promise.resolve(config)
+
+        while (chain.length) {
+            const { resolved, rejected } = chain.shift()!
+            promise = promise.then(resolved, rejected)
+
+        }
+        return promise
     }
 
     get(url: string, config?: AxiosRequestConfig): AxiosPromise {
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,9 @@
 // Axios类
 export interface Axios {
+  interceptors: {
+    request: AxiosInterceptorManager<AxiosRequestConfig>
+    response: AxiosInterceptorManager<AxiosResponse>
+  }
   request<T = any>(config: AxiosRequestConfig): AxiosPromise<T>
 
   get<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
@@ -64,3 +68,18 @@ export interface AxiosInstance extends Axios {
 
   <T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
 }
+
+export interface AxiosInterceptorManager<T> {
+  use(resolved: ResolvedFn<T>, rejected?: RejectedFn): number
+
+  eject(id: number): void
+}
+
+export interface ResolvedFn<T> {
+  (val: T): T | Promise<T>
+}
+
+export interface RejectedFn {
+  (val: any): any
+}
+
